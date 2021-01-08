a43e6e9b1e445f670b818fd7f185428baf619081
DashaDasha88 <dasha.kotova.mail@gmail.com>
2021-01-08 20:06:58 +0000

    Two practice files

---
+++ tinkering.js
@@ -0,0 +1,7 @@
+var x = 10;
+while (x <= 25) {
+  console.log('Printing out x = ' + x);
+  x = x + 2;
+}
+
+console.log('Printing out x = ' + x);
---
+++ whichschool.js
@@ -0,0 +1,13 @@
+const whichSchool = function (age){
+  if (age >= 1 && age <= 12) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else if (age >= 19 && age <= 100) {
+    return "Lighthouse Labs";
+  }
+}
+
+console.log(whichSchool(35));
+console.log(whichSchool(8));
+console.log(whichSchool(13));
